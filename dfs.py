c75f2d078899f44811fc059833cd096b55c229c3
Harshaan Babra <harshaanniles010@gmail.com>
2022-12-04 01:58:44 -0800

    Add files via upload

---
+++ dfs.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return "Node(" + str(self.value) + ")"
+
+
+def walk1(tree):
+    if tree is not None:
+        print(tree)
+        walk(tree.left)
+        walk(tree.right)
+
+
+def walk2(tree, stack):
+    stack.append(tree)
+    while len(stack) > 0:
+        node = stack.pop()
+        if node is not None:
+            print(node)
+            stack.append(node.right)
+            stack.append(node.left)
